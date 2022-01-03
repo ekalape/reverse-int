43834fa00a9efb911496d37729db9ffcb1c45e52
Ekalape <ekalape@outlook.it>
2022-01-03 15:10:04 +0100

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let str = Math.abs(n).toString().split("");
+
+    return Number(str.reverse().join(""));
+};
